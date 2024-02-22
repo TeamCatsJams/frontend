6fdaea50ced509bb8d8e4c5b921862090b15830e
Tweedle2Dum <utkarsh.work1405@gmail.com>
2024-02-22 21:51:16 +0530

    feat. complete search layout, add. template cards

--- Components/UI/Cards.tsx
+++ Components/UI/Cards.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { Avatar, Box, Container, Stack, Typography } from "@mui/material";
 
-type Props = {}
+type Props = {};
 
 function Cards({}: Props) {
   return (
-    <div>Cards</div>
-  )
+    <>
+      <Box
+        minHeight={"350px"}
+        maxHeight={"400px"}
+        minWidth={"200px"}
+        maxWidth={"300px"}
+        borderRadius={"16px"}
+        sx={{
+          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        {" "}
+        <Container>
+          <Stack alignItems={"center"} p={"20px"} gap={4}>
+            <Avatar
+              alt="UtkarsH SRIVASTAVA"
+              sx={{ height: "150px", width: "200px", borderRadius: "16px" }}
+            />
+
+            <Typography>Name</Typography>
+            <Typography color={'primary'}>Designation</Typography>
+            <Typography color={'primary'}>Location of work</Typography>
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
 }
 
-export default Cards
+export default Cards;
