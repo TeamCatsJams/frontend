b51db70986e2aa65d9735e12b1ea59a4a0dac017
Tweedle2Dum <utkarsh.work1405@gmail.com>
2024-02-23 02:14:03 +0530

    add. authprovider, googleoauth

--- Components/Providers/AuthProvider.tsx
+++ Components/Providers/AuthProvider.tsx
@@ -1,11 +1,101 @@
-import React from 'react'
-
-type Props = {}
-
-function AuthProvider({}: Props) {
-  return (
-    <div>AuthProvider</div>
-  )
+//@ts-nocheck
+import React, { createContext, useContext, useEffect, useState } from "react";
+import {
+  GoogleAuthProvider,
+  User,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../../utils/firebase.utils";
+interface CustomUser {
+  token: string;
+  user: User;
 }
 
-export default AuthProvider
+// Create a context for the authentication provider
+interface AuthContextProps {
+  user: CustomUser | null;
+  login: () => Promise<void>;
+  logout: () => Promise<void>;
+  // Add any other values or functions you want to expose
+}
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<CustomUser | null>(null);
+  async function login() {
+    try {
+      const result = await signInWithPopup(auth, new GoogleAuthProvider());
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential?.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+
+      setUser({ user: user, token: token as string });
+    } catch (error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      // The email of the user's account used.
+      const email = error.customData.email;
+
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+    }
+  }
+
+  async function logout() {
+    try {
+      await signOut(auth);
+      setUser(null);
+    } catch (error) {
+      console.error("Error during logout:", error);
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const token = await user.getIdToken();
+        setUser({ user, token });
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  // Provide the auth context value to the children
+  const contextValue: AuthContextProps = {
+    user,
+    login,
+    logout,
+    // Add any other values or functions you want to expose
+  };
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
--- utils/firebase.utils.ts
+++ utils/firebase.utils.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getAuth} from "firebase/auth"
 // TODO: Add SD Ks for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,7 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
-const analytics = getAnalytics(app);
 
-export default {app,auth}
+export {app}
+export {auth}
 
